744416868f4472ec8baee20698cdde13ccafbe7c
Yuehuan Lu <yuehuan.lu@YuehuandeMacBook-Pro.local>
2021-02-05 15:21:23 +0800

    [luyuehuan] feat:tableView Cell

---
+++ WeChat-2/TweetsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeChat-2
+//
+//  Created by Yuehuan Lu on 2021/2/5.
+//
+
+import UIKit
+
+class TweetsViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ WeChat-2/View/TweetCell.swift
@@ -0,0 +1,16 @@
+//
+//  TweetCell.swift
+//  WeChat-2
+//
+//  Created by Yuehuan Lu on 2021/2/5.
+//
+
+import Foundation
+import UIKit
+
+class TweetCell: UITableViewCell {
+  @IBOutlet weak var avatarImage: UIImageView!
+  @IBOutlet weak var nickLabel: UILabel!
+  @IBOutlet weak var contentLabel: UILabel!
+  @IBOutlet weak var collectionView: UICollectionView!
+}
